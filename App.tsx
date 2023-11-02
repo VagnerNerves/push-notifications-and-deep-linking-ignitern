72022dcb253157f2d0c05756bf316eee08fe71e6
Vagner Nerves <vagnernervessantos@gmail.com>
2023-11-02 08:55:57 -0300

    feat: Created for get click on notification.

--- App.tsx
+++ App.tsx
@@ -18,18 +18,39 @@ import {
   tagUserInfoCreate,
   tagUserInfoRemove,
 } from "./src/notifications/notificationsTags";
+import { useEffect } from "react";
 
 OneSignal.initialize("86932e3b-31a2-4264-ac71-5b60a502ea3b");
 //OneSignal.User.addEmail("vagnernervessantos@gmail.com");
 
 OneSignal.Notifications.requestPermission(true);
-//6 Push Notifications - (17) Refatorando o componente de Notificação _ Rocketseat.mp4
+//6 Push Notifications - (19) Verificando clique na notificação em background _ Rocketseat
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
   tagUserInfoCreate();
   // tagUserEmailRemove();
+
+  useEffect(() => {
+    const unsubscribe = OneSignal.Notifications.addEventListener(
+      "click",
+      (response) => {
+        const { actionId } = response.result;
+
+        switch (actionId) {
+          case "1":
+            return console.log("Ver todas");
+          case "2":
+            return console.log("Ver pedidos");
+          default:
+            return console.log("Não foi clicado em botão de ação");
+        }
+      }
+    );
+
+    return () => unsubscribe;
+  }, []);
 
   return (
     <NativeBaseProvider theme={THEME}>

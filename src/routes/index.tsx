184929f713b8c4f3d959e8c399e27abfecebfaee
Vagner Nerves <vagnernervessantos@gmail.com>
2023-11-03 08:51:14 -0300

    feat: add config Linking.

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -6,6 +6,24 @@ import { OneSignal, OSNotification } from "react-native-onesignal";
 import { AppRoutes } from "./app.routes";
 import { Notification } from "../components/Notification";
 
+const linking = {
+  prefixes: [
+    "com.vagnernerves.igniteshoes://",
+    "igniteshoesapp://",
+    "exp+igniteshoesapp://",
+  ],
+  config: {
+    screens: {
+      details: {
+        path: "details/:productId",
+        parse: {
+          productId: (productId: string) => productId,
+        },
+      },
+    },
+  },
+};
+
 export function Routes() {
   const [notification, setNotification] = useState<OSNotification>();
   const { colors } = useTheme();
@@ -27,7 +45,7 @@ export function Routes() {
   }, []);
 
   return (
-    <NavigationContainer theme={theme}>
+    <NavigationContainer theme={theme} linking={linking}>
       <AppRoutes />
 
       {notification?.title && (
